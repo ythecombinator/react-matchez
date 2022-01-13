9e02fb6d9e20875feeb414d4cf3218e601485147
Matheus Albuquerque <land@ythecombinator.space>
2022-01-13 23:06:36 +0100

    feat(src/_internals): bootstrap error handling

---
+++ src/_internals/error.ts
@@ -0,0 +1,25 @@
+export const exceptions = {
+  match: {
+    no_children: `<Match> was provided with no children. Valid children are either <With> or <When> components.`,
+    no_otherwise: `<Match> was provided with no otherwise case. A valid otherwise case should be provided either using the 'otherwise' prop or the <Otherwise> component.`,
+    multiple_otherwise: `<Match> was provided with multiple otherwise cases.`,
+    invalid_children: `<Match> was provided with invalid children. Valid children are: <With>, <When> or <Otherwise>.`,
+  },
+  with: {
+    no_shape: `<With> was provided with no shape props to be matched.`,
+    no_children: `<With> was provided with no children to be rendered.`,
+  },
+  when: {
+    no_predicate: `<When> was provided with no predicate prop.`,
+    no_children: `<When> was provided with no children to be rendered.`,
+  },
+  otherwise: {
+    no_children: `<Otherwise> was provided with no children to be rendered.`,
+  },
+};
+
+export const invariant = (condition: boolean, exception: string) => {
+  if (condition) {
+    throw new Error(`react-match invariant violation: ${exception}`);
+  }
+};
