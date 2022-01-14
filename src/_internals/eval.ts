01bd7ef1411d5fbe06fb0d3306c401fa08069f33
Matheus Albuquerque <land@ythecombinator.space>
2022-01-14 12:06:01 +0100

    feat(src/_internals/eval): initial `With` exp evaluation

--- src/_internals/eval.ts
+++ src/_internals/eval.ts
@@ -1,5 +1,12 @@
-import { ElementWithMetadata, ElementWithMetadataUnion } from './types';
+import { isMatching } from 'ts-pattern';
+
+import {
+  ElementWithMetadata,
+  ElementWithMetadataUnion,
+  MatchWithCase,
+} from './types';
 import { When, WhenProps } from '../components/When';
+import { With } from '../components/With';
 
 // WHEN
 
@@ -26,4 +33,44 @@ export function evaluateWhenExpression<Shape>(
     element: item.element,
     position: item.position,
   }));
+}
+
+// WITH
+
+export function isWith<Shape>(
+  child: ElementWithMetadataUnion<Shape>
+): child is ElementWithMetadata<Shape> {
+  return child.element.type === With;
+}
+
+export function withMatcher<Shape>(value: Shape) {
+  return (instance: MatchWithCase<Shape>) => {
+    const isMatch = isMatching(value, instance.pattern);
+    return isMatch;
+  };
+}
+
+export function evaluateWithExpression<Shape>(
+  childrenArr: ElementWithMetadata<Shape>[],
+  value: Shape
+) {
+  if (childrenArr.length === 0) {
+    return [];
+  }
+
+  const cases = childrenArr.map((children) => {
+    const { children: childrenChild, ...childrenPattern } =
+      children.element.props;
+
+    return {
+      element: childrenChild,
+      position: children.position,
+      pattern: childrenPattern,
+    } as MatchWithCase<Shape>;
+  });
+
+  return cases.filter(withMatcher(value)).map((item) => ({
+    element: item.element,
+    position: item.position,
+  })) as Array<ElementWithMetadata<Shape>>;
 }
